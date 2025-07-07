74d54bbeca2dbfdf57d30274429b86639f1d9756
Gabriel Pinho <gabriel.admpinho@gmail.com>
2025-07-07 16:14:33 -0300

    :sparkles: criacao e adicao de motoqueiro como objeto

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import Player from './utils/Player.js';
+
 let config = {
   renderer: Phaser.AUTO,
   width: 840,
@@ -5,8 +7,8 @@ let config = {
   physics: {
     default: 'arcade',
     arcade: {
-        gravity: { y: 300 },
-        debug: false
+      gravity: { y: 300 },
+      debug: false
     }
   },
   scene: {
@@ -25,92 +27,60 @@ let isGameStarted = false;
 let messageToPlayer;
 let animationTimer = 0;
 let currentFrame = 0;
-let frameNames = ['run1', 'run2'];
 let faseAtual = 1;
 let background;
 
-function preload () {
+function preload() {
   this.load.image('background', 'assets/mapOne Style Game Map.png');
+  this.load.image('biker', 'assets/biker/biker.png'); 
+  this.load.image('biker-left', 'assets/biker/biker-left.png'); 
   this.load.image('back2', 'assets/back2.png'); //TESTE para mapTwo (em produção)
   this.load.image('back3', 'assets/back2.png'); //TESTE para mapThree (em produção)
-  this.load.image('run1', 'assets/run1.png');
-  this.load.image('run2', 'assets/run2.png');
 }
 
-function create () {
+function create() {
   background = this.add.image(0, 0, 'background').setOrigin(0, 0);
   background.setDisplaySize(840, 480);
-  run1 = this.physics.add.sprite(50, 250, 'run1').setScale(0.025);
-  run1.setCollideWorldBounds(true);
+  run1 = new Player(this, 50, 250, 'biker');
+
   cursors = this.input.keyboard.createCursorKeys();
 }
 
-function update (time, delta) {
+function update(time, delta) {
   run1.body.setAllowGravity(false);
   if (cursors.space.isDown && !isGameStarted) {
     isGameStarted = true;
-    //messageToPlayer.text = 'Aperte a tecla ↑ para mover Zé para cima.';
+    // messageToPlayer.text = 'Aperte a tecla ↑ para mover Zé para cima.';
   }
   if (!isGameStarted) {
     run1.setVelocityY(0);
   }
-  if (cursors.up.isDown && !hasLanded && !hasBumped) {
-    if(faseAtual === 1){
-      run1.setVelocityY(-50); 
-    }else if(faseAtual === 2){
-      run1.setVelocityY(-150); 
-    }else{
-      run1.setVelocityY(-200); 
-    }
-  }
-  if (cursors.down.isDown && !hasLanded && !hasBumped){
-    if(faseAtual === 1){
-      run1.setVelocityY(50);
-    }else if(faseAtual === 2){
-      run1.setVelocityY(150);
-    }else{
-      run1.setVelocityY(200);
-    }
-  }
-  if (isGameStarted && (!hasLanded && !hasBumped)) {
-    if(faseAtual === 1){
-      run1.body.velocity.x = 30;
-    }else if(faseAtual === 2){
-      run1.body.velocity.x = 60;
-    }else{
-      run1.body.velocity.x = 90;
-    }
-    animationTimer += delta;
-    if (animationTimer > 150) {
-      currentFrame = (currentFrame + 1) % frameNames.length;
-      run1.setTexture(frameNames[currentFrame]);
-      animationTimer = 0;
-    }
+
+  if (isGameStarted && !hasLanded && !hasBumped) {
+    run1.update(cursors, delta, faseAtual);
   } else {
-    run1.body.velocity.x = 0;
-    run1.setTexture('run1');
+    run1.setVelocity(0, 0);
+    run1.setTexture('biker');
   }
-  if (hasLanded || hasBumped) {
-    //messageToPlayer.text = 'Zé entregador bateu!';
-  }
+
   if (faseAtual === 1 && run1.x > 800 && !hasBumped) {
     faseDois(this);
   }
-  if (faseAtual === 2 && run1.x > 800 && !hasBumped){
+  if (faseAtual === 2 && run1.x > 800 && !hasBumped) {
     faseTres(this);
   }
-  
+
 }
 
 function faseDois(scene) {
   faseAtual = 2;
-  background.setTexture('back2');
+  background.setTexture('biker');
   background.setDisplaySize(840, 480);
   hasLanded = false;
   hasBumped = false;
   isGameStarted = true;
   run1.setPosition(0, 250);
-  run1.setTexture('run1');
+  run1.setTexture('biker');
 
   //messageToPlayer.text = 'Fase 2: Continue entregando!';
   //Phaser.Display.Align.In.BottomCenter(messageToPlayer, background, 150, 300);
@@ -125,7 +95,7 @@ function faseTres(scene) {
   isGameStarted = true;
   run1.setPosition(0, 250);
   //run1.setVelocity(0, 0);
-  run1.setTexture('run1');
+  run1.setTexture('biker');
 
   //messageToPlayer.text = 'Fase 3: Parabéns! Você chegou até a última fase. Mantenha o foco!';
   //Phaser.Display.Align.In.BottomCenter(messageToPlayer, background, 150, 300);
---
+++ utils/Player.js
@@ -0,0 +1,57 @@
+export default class Player extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y, texture) {
+    super(scene, x, y, texture);
+
+    // Adiciona o jogador na cena com física
+    scene.add.existing(this);
+    scene.physics.add.existing(this);
+
+    this.setScale(0.2);
+    this.setCollideWorldBounds(true);
+
+    this.currentFrame = 0;
+    this.animationTimer = 0;
+  }
+
+  update(cursors, delta, faseAtual) {
+    let speedX = 0;
+    let speedY = 0;
+
+    // Define velocidade baseada na fase
+    if (faseAtual === 1) {
+      speedX = 100;
+      speedY = 75;
+    } else if (faseAtual === 2) {
+      speedX = 150;
+      speedY = 100;
+    } else {
+      speedX = 200;
+      speedY = 150;
+    }
+
+    this.setVelocity(0, 0);
+
+    // Movimento horizontal
+    if (cursors.left.isDown) {
+      this.setVelocityX(-speedX);
+      this.setTexture('biker-left')
+    } else if (cursors.right.isDown) {
+      this.setTexture('biker')
+      this.setVelocityX(speedX);
+    }
+
+    // Movimento vertical
+    if (cursors.up.isDown) {
+      this.setVelocityY(-speedY);
+    } else if (cursors.down.isDown) {
+      this.setVelocityY(speedY);
+    }
+
+  }
+
+  reset(x, y) {
+    this.setPosition(x, y);
+    this.setVelocity(0, 0);
+    this.setTexture('biker');
+  }
+}
