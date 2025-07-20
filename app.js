7d615463987a29930fcf9537a56484220bead12c
Gabriel Pinho <gabriel.admpinho@gmail.com>
2025-07-19 21:00:33 -0300

    :bug: correcao de bug de fisica de persoangem

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ let config = {
     default: 'arcade',
     arcade: {
       gravity: { y: 0 },
-      debug: false
+      debug: true
     }
   },
   scene: {
@@ -32,15 +32,32 @@ function preload() {
   this.load.image('ground', 'assets/city/ground.png');
   this.load.image('street-h', 'assets/city/street-h.png');
   this.load.image('street-v', 'assets/city/street-v.png');
-  this.load.image('biker-right', 'assets/biker/biker-right.png'); 
-  this.load.image('biker-left', 'assets/biker/biker-left.png'); 
-  this.load.image('biker-up', 'assets/biker/biker-up.png'); 
-  this.load.image('biker-down', 'assets/biker/biker-down.png'); 
+
+  this.load.image('biker-right', 'assets/biker/biker-right.png');
+  this.load.image('biker-left', 'assets/biker/biker-left.png');
+  this.load.image('biker-up', 'assets/biker/biker-up.png');
+  this.load.image('biker-down', 'assets/biker/biker-down.png');
+
+  this.load.image('store', 'assets/city/store.png');
 }
 
 function create() {
   background = this.add.tileSprite(0, 0, screenWidth, screenHeight, 'ground').setOrigin(0, 0);
   player = new Player(this, 50, 250, 'biker-right');
+  
+  const lojas = [
+    { x: 0, y: 0, width: 1200, height: 50 },
+    { x: 0, y: 350, width: 400, height: 150 },
+    { x: 610, y: 150, width: 50, height: 50 },
+    { x: 500, y: 150, width: 50, height: 50 },
+    { x: 500, y: 150, width: 50, height: 50 }
+  ];
+
+  lojas.forEach(loja => {
+    const hitbox = this.add.rectangle(loja.x, loja.y, loja.width, loja.height);
+    this.physics.add.existing(hitbox, true);
+    this.physics.add.collider(player, hitbox);
+  });
 
   cursors = this.input.keyboard.createCursorKeys();
 }
--- utils/Player.js
+++ utils/Player.js
@@ -8,36 +8,59 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
 
     this.setScale(0.2);
     this.setCollideWorldBounds(true);
-
+    this.body.setSize(250, 80).setOffset(200, 200);
     this.currentFrame = 0;
     this.animationTimer = 0;
   }
 
   update(cursors) {
-    let speedX = 0;
-    let speedY = 0;
+    let velocityX = 0;
+    let velocityY = 0;
+    const speedX = 100;
+    const speedY = 75;
 
-    speedX = 100;
-    speedY = 75;
-    this.setVelocity(0, 0);
+    let direction = null;
 
-    // Movimento vertical
     if (cursors.up.isDown) {
-      this.setVelocityY(-speedY);
-      this.setTexture('biker-up')
+      velocityY = -speedY;
+      direction = 'up';
     } else if (cursors.down.isDown) {
-      this.setTexture('biker-down')
-      this.setVelocityY(speedY);
+      velocityY = speedY;
+      direction = 'down';
     }
-    // Movimento horizontal
     if (cursors.left.isDown) {
-      this.setVelocityX(-speedX);
-      this.setTexture('biker-left')
+      velocityX = -speedX;
+      direction = direction || 'left';
     } else if (cursors.right.isDown) {
-      this.setTexture('biker-right')
-      this.setVelocityX(speedX);
+      velocityX = speedX;
+      direction = direction || 'right';
+    }
+
+    // Normalizacao de velocidade diagonal
+    if (velocityX !== 0 && velocityY !== 0) {
+      const norm = Math.SQRT1_2;
+      velocityX *= norm;
+      velocityY *= norm;
+    }
+
+    this.setVelocity(velocityX, velocityY);
+
+    // Textura e hitbox
+    if (direction === 'up') {
+      this.setTexture('biker-up');
+      this.body.setSize(100, 150).setOffset(250, 100);
+    } else if (direction === 'down') {
+      this.setTexture('biker-down');
+      this.body.setSize(100, 150).setOffset(250, 100);
+    } else if (direction === 'left') {
+      this.setTexture('biker-left');
+      this.body.setSize(250, 80).setOffset(200, 200);
+    } else if (direction === 'right') {
+      this.setTexture('biker-right');
+      this.body.setSize(250, 80).setOffset(200, 200);
     }
   }
+
 
   reset(x, y) {
     this.setPosition(x, y);
