0437966b12dfef58b7be37b44db7573c1b8cdcaa
Gabriel Pinho <gabriel.admpinho@gmail.com>
2025-08-05 02:00:44 -0300

    added paper boxes

--- app.js
+++ app.js
@@ -41,6 +41,8 @@ function preload() {
   this.load.image('car-left', 'assets/car/car-left.png');
   this.load.image('car-up', 'assets/car/car-up.png');
   this.load.image('car-down', 'assets/car/car-down.png');
+
+  this.load.image('paper-bag', 'assets/paper-bag.png');
 }
 
 function create() {
@@ -75,22 +77,80 @@ function create() {
     { x: 1140, y: 260, width: 50, height: 500 },
     { x: 1140, y: 10, width: 150, height: 20 },
   ];
+  const deliveryPoints = [
+    { x: 300, y: 180, width: 80, height: 60 },
+    { x: 765, y: 380, width: 70, height: 50 },
+  ]
 
-  mapBlocks.forEach(mapBlock => {
-    const hitbox = this.add.rectangle(mapBlock.x, mapBlock.y, mapBlock.width, mapBlock.height);
+  deliveryPoints.forEach(deliveryPoint => {
+    const { x, y, width, height } = deliveryPoint;
+
+    const paperBagArea = (deliveryPoint.width * deliveryPoint.height) / 2;
+    // Group to hold all bags
+    const bagCount = 1;
+    const paperBags = this.add.group();
+
+    for (let i = 0; i < bagCount; i++) {
+      const bagX = Phaser.Math.Between(x, x + width);
+      const bagY = Phaser.Math.Between(y, y + height);
+
+      const bag = this.add.image(bagX, bagY, 'paper-bag')
+        .setScale(0.1)
+        .setAlpha(0.8).
+        bag.setDepth(5); // anything higher than background or hitbox graphics
+
+      this.tweens.add({
+        targets: bag,
+        y: bag.y  - 10,
+        duration: Phaser.Math.Between(1000, 2000),
+        yoyo: true,
+        repeat: -1,
+        ease: 'Sine.easeInOut',
+        delay: Phaser.Math.Between(0, 500)
+      });
+
+    }
+
+
+    // Desenha a área visível (posição: canto superior esquerdo)
+    const graphics = this.add.graphics();
+    graphics.fillStyle(0x00ff00, 0.3); // verde com opacidade
+    graphics.fillRect(x, y, width, height); // aqui continua igual
+
+    // Calcula centro para a hitbox (posição centralizada)
+    const centerX = x + width / 2;
+    const centerY = y + height / 2;
+
+    const hitbox = this.add.rectangle(centerX, centerY, width, height);
     this.physics.add.existing(hitbox, true);
     this.physics.add.collider(player, hitbox);
-    // Redireciona carros
+
     cars.forEach(car => {
-      this.physics.add.collider(player, car);
       this.physics.add.collider(car, hitbox, () => {
         car.reverseDirection();
       });
     });
   });
 
+
   cursors = this.input.keyboard.createCursorKeys();
 }
+
+mapBlocks.forEach(mapBlock => {
+  const hitbox = this.add.rectangle(mapBlock.x, mapBlock.y, mapBlock.width, mapBlock.height);
+  this.physics.add.existing(hitbox, true);
+  this.physics.add.collider(player, hitbox);
+  // Redireciona carros
+  cars.forEach(car => {
+    this.physics.add.collider(player, car);
+    this.physics.add.collider(car, hitbox, () => {
+      car.reverseDirection();
+    });
+  });
+});
+
+cursors = this.input.keyboard.createCursorKeys();
+
 
 function update(delta) {
   if (cursors.space.isDown && !isGameStarted) {
